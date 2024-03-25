6c7f1a6a22632acf94f455f92f2fa6d26ab4bfd3
Jakob Strøm <jakob@shareonline.dk>
2024-03-25 13:29:00 +0100

    update

--- _sub/containers/resourcegroup/outputs.tf
+++ _sub/containers/resourcegroup/outputs.tf
@@ -1,6 +1,4 @@
 output "resource_group_name" {
   description = "name of the resource group"
-  value = {
-    resource_group_name = azurerm_resource_group.main.name
-  }
+  value = azurerm_resource_group.main.name
 }
