86985a674512de982160a37a67104cb4a5286203
Jakob Strøm <jakob@shareonline.dk>
2024-04-08 09:11:53 +0200

    update sa account

--- _sub/storage/storage-account/main.tf
+++ _sub/storage/storage-account/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "main" {
-  name                             = var.sa_name
+  name                             = replace(var.sa_name, "-", "")
   location                         = var.location
   resource_group_name              = var.resource_group_name
   account_tier                     = "Standard"
