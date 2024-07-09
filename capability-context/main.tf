69a0ce118385a9089d07dddc6b4d1451a34d268a
Emil H. Clausen <sequoiia@hummel.yt>
2024-07-09 15:08:37 +0200

    Changed default resource names due to length issues temporarily

--- capability-context/main.tf
+++ capability-context/main.tf
@@ -45,7 +45,7 @@ resource "azurerm_role_assignment" "resourcegroup-capability" {
 
 module "keyvault" {
   source   = "../_sub/security/keyvault"
-  name                          = "${var.capability_id}_capability-default"
+  name                          = "${var.capability_id}_${var.environment}"
   tenant_id                     = var.tenant_id
   location                      = "westeurope"
   resource_group_name           = module.resourcegroup.resource_group_name
@@ -57,7 +57,7 @@ module "keyvault" {
 }
 module "storage_account" {
   source   = "../_sub/storage/storage-account"
-  sa_name                 = "${var.capability_id}_capability-default"
+  sa_name                 = "${var.capability_id}_${var.environment}"
   location                = "westeurope"
   resource_group_name     = module.resourcegroup.resource_group_name
   ad_group_id             = module.adgroup.group_id
