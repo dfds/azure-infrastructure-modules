18102bd3b9504a20b69dd1831b78ba72bf9830d5
Jakob Strøm <jakob@shareonline.dk>
2024-07-15 00:43:16 +0200

    update

--- _sub/security/ad-group/outputs.tf
+++ _sub/security/ad-group/outputs.tf
@@ -2,5 +2,5 @@
 #   value = azuread_group.main.id
 # }
 output "group_id" {
-  value = length(data.azuread_groups.capability_ssu_group) > 0 ? data.azuread_groups.capability_ssu_group[0].id : azuread_group.adgroup[0].id
+  value = length(data.azuread_groups.capability_ssu_group) > 0 ? data.azuread_groups.capability_ssu_group[0].id : azuread_group.main[0].id
 }
