712c86597d968c4dcf3cd64204b611c2a695f12f
Jakob Strøm <jakob@shareonline.dk>
2024-07-15 01:03:45 +0200

    update

--- _sub/security/ad-group/main.tf
+++ _sub/security/ad-group/main.tf
@@ -1,5 +1,5 @@
 resource "azuread_group" "main" {
-  count = length(data.azuread_groups.capability_ssu_group) == 0 ? 1 : 0
+  count = length(data.azuread_groups.capability_ssu_group.object_ids) == 0 ? 1 : 0
   display_name     = var.displayname
   security_enabled = true
   assignable_to_role = false
