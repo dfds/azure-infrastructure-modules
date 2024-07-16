35899f6813652790e8629d1d528fadd4d161813e
Jakob Strøm <jakob@shareonline.dk>
2024-07-16 02:38:42 +0200

    update

--- _sub/containers/resourcegroup/main.tf
+++ _sub/containers/resourcegroup/main.tf
@@ -15,15 +15,13 @@ locals {
   current_date = timestamp()
 
   # Extract the year and month, and construct the first day of the current month
-  year  = formatdate("2006", local.current_date)
-  month = formatdate("01", local.current_date)
-
-  # Construct the start date as the first day of the current month
-  start_date = "${local.year}-${local.month}-01T00:00:00Z"
+  start_date_prefix = formatdate("2006-01", local.current_date)
+  start_date = "${start_date_prefix}-01T00:00:00Z"
 
   # Add 12 months to the start date to get the end date
-  end_date = formatdate("2006-01-02T15:04:05Z", timeadd(local.start_date, "8760h")) # 8760 hours in 12 months
+  end_date = timeadd(local.start_date, "8760h") # 8760 hours in 12 months
 }
+
 resource "azurerm_consumption_budget_resource_group" "main" {
   name                = "${azurerm_resource_group.main.name}-budget"
   resource_group_id  = azurerm_resource_group.main.id
