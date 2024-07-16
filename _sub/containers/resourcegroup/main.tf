c5e6d6fef506c53abb3d5e7592a37b9ebb83244c
Jakob Strøm <jakob@shareonline.dk>
2024-07-16 03:18:38 +0200

    update

--- _sub/containers/resourcegroup/main.tf
+++ _sub/containers/resourcegroup/main.tf
@@ -12,12 +12,12 @@ resource "azurerm_resource_group" "main" {
 
 locals {
   # Get the current date and time
-  current_date = timestamp()
+  current_timestamp = timestamp()
 
   # Extract the current year and month, and construct the first day of the current month
-  current_year  = formatdate("2006", local.current_date)
-  current_month = formatdate("01", local.current_date)
-  start_date = "${current_year}-${current_month}-01T00:00:00Z"
+  current_year  = formatdate("2006", local.current_timestamp)
+  current_month = formatdate("01", local.current_timestamp)
+  start_date    = "${local.current_year}-${local.current_month}-01T00:00:00Z"
 
   # Add 12 months to the start date to get the end date
   end_date = timeadd(local.start_date, "8760h") # 8760 hours in 12 months
