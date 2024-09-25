5f95b15ce5afd7a1421ba82036872bdf8614bfa0
Jakob Strom <jakob@shareonline.dk>
2024-09-25 12:29:09 +0200

    bugfix in provider versions

--- capability-context/providers.tf
+++ capability-context/providers.tf
@@ -7,6 +7,7 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
   tenant_id       = "73a99466-ad05-4221-9f90-e7142aa2f6c1"
+  //skip_provider_registration = true
 }
 
 provider "azuread" {
